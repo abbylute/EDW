110484fb123d94ff3989d0d965fb72a5376931c9
Abby <aby.lute@gmail.com>
2018-03-20 10:27:51 -0700

    Changed to allow for a vector of buffer lengths

--- Code/calculate_station_tpi.R
+++ Code/calculate_station_tpi.R
@@ -2,22 +2,21 @@
 
 calculate_station_tpi = function(meta, buffer){
 
-library(raster)
-#library(spatialEco)
-#library(sp)
+  library(raster)
+  
+  m <- raster('Data/DEM/WUS_DEM.tif')
+  
+  # find the grid cell in which each station is located and calculate the tpi at a range of scales:
+  myPoints <- data.frame(meta$lon, meta$lat)
+  coordinates(myPoints) <- c('meta.lon', 'meta.lat')
+  proj4string(myPoints) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
+  
+  out <- data.frame(matrix(NA,nrow=nrow(meta),ncol=length(buffer)))
 
-m <- raster('Data/DEM/WUS_DEM.tif')
-#nlat <- dim(m)[1]
-#nlon <- dim(m)[2]
-#r<- crop(m, extent(sub_bbox[1],sub_bbox[3],sub_bbox[2],sub_bbox[4]))
-
-# find the grid cell in which each station is located and calculate the tpi at a range of scales:
-myPoints <- data.frame(meta$lon, meta$lat)
-coordinates(myPoints) <- c('meta.lon', 'meta.lat')
-proj4string(myPoints) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
-
-meta$elev - extract(m, myPoints, buffer=buffer, fun=mean, cellnumbers=T) # the buffer units appears to be meters
-
+  for (ii in 1:length(buffer)){
+    out[,ii] <- meta$elev - extract(m, myPoints, buffer=buffer[ii], fun=mean, cellnumbers=T) # the buffer units appears to be meters
+  }
+    
 }
 
 
