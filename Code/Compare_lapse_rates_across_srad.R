a15f4e03023bac2be7574ef8a58990af052f7dff
Abby <aby.lute@gmail.com>
2018-04-02 10:09:27 -0700

    Created scripts to compare lapse rates across different subsets of stations.

---
+++ Code/Compare_lapse_rates_across_srad.R
@@ -0,0 +1,77 @@
+pertab <- data.frame(matrix(ncol=3,nrow=4))
+names(pertab) <- c('season','dmean','dsd')
+pertab$season <- c(sets$season)
+
+da <- data.frame()
+dl <- data.frame()
+
+for (ss in 1:4){ # for each season:
+  sm <- season_means %>% filter(season==unique(seasons$season[ss]))
+  thres <- median(sm$srad)
+  
+  out <- sm %>% filter(srad >thres) %>% group_by(year,season) %>%
+    summarise(lapse_high=summary(lm(smtemp~elev))$coefficients[2]*1000)
+  
+  out2 <- sm %>% filter(srad<thres) %>% group_by(year,season) %>%
+    summarise(lapse_low=summary(lm(smtemp~elev))$coefficients[2]*1000)
+  
+  outs <- left_join(out,out2, by=c('season','year'))
+  
+  highlow <- outs %>% group_by(season) %>% summarise(mhigh=mean(lapse_high),mlow=mean(lapse_low),
+                                                     shigh=sd(lapse_high),slow=sd(lapse_low))
+  da <- rbind(da, highlow %>% group_by(season) %>%
+    summarise(dmean=mhigh-mlow, dsd=shigh-slow) %>% gather('metric','value',dmean:dsd))
+  
+
+  diflapse <- data.frame(matrix(ncol=3,nrow=0))
+  names(diflapse) <- c('season','dmean','dsd')
+  dd <- sm %>% filter(srad>thres) 
+  sts <- length(unique(dd$station_id))
+  for (ii in 1:1000){
+    s1 <- unique(sm$station_id) %>% sample(sts)
+    set1 <- sm %>% filter(station_id %in% s1) %>% group_by(season,year) %>%
+      mutate(lapse=summary(lm(smtemp~elev))$coefficients[2]*1000) %>% group_by(season) %>%
+      summarise(mlapse1=mean(lapse,na.rm=T),slapse1=sd(lapse,na.rm=T))
+    set2 <- sm %>% filter(!station_id %in% s1) %>% group_by(season,year) %>%
+      mutate(lapse=summary(lm(smtemp~elev))$coefficients[2]*1000) %>% group_by(season) %>%
+      summarise(mlapse2=mean(lapse,na.rm=T),slapse2=sd(lapse,na.rm=T))
+    sets <- left_join(set1,set2,by='season')
+    diflapse <- bind_rows(diflapse, (sets %>% group_by(season) %>% summarise(dmean=mlapse1-mlapse2,dsd=slapse1-slapse2)))
+  }
+  
+  dl <- rbind(dl, diflapse %>% gather('metric','value',dmean:dsd))
+}
+
+
+  plot_names <- list('dmean'="Difference in mean value",'dsd'="Difference in standard deviation")
+  plot_labeller <- function(variable,value){
+    return(plot_names[value])
+  }
+  
+  ggplot(dl, aes(season,(value))) + geom_boxplot() + facet_wrap(~metric, ncol=1, scales="free",labeller=plot_labeller) +
+    geom_point(inherit.aes=F, data=da, aes(x=season,y=(value),col='red'),show.legend=F) +
+    labs(y='C/km',x='Season',title=paste0('Comparing lapse rates across solar radiation classes'))
+  #ggsave(paste0('Figures/dLapse_across_TPI_',thistpi,'.jpeg'))
+  
+  
+  for(seas in 1:4){
+    ii <- which(pertab$tpi==thistpi & pertab$season==unique(dl$season)[seas])
+    percentile <- ecdf(dl$value[which(dl$metric=='dmean' & dl$season==unique(dl$season)[seas])])
+    pertab$dmean[ii] <- percentile(da$value[which(da$metric=='dmean' & da$season==unique(dl$season)[seas])])
+    
+    percentile <- ecdf(dl$value[which(dl$metric=='dsd' & dl$season==unique(dl$season)[seas])])
+    pertab$dsd[ii] <- percentile(da$value[which(da$metric=='dsd' & da$season==unique(dl$season)[seas])])
+    
+  }
+} 
+
+#write.table(pertab, 'Figures/dLapse_percentiles.txt')
+
+# calculate the average percentile of the mean difference and sd difference (across all seasons, all tpis):
+pertab %>% summarise(mlapse=mean(dmean),slapse=mean(dsd))
+# calculate the mean percentiles across seasons:
+pertab %>% group_by(season) %>% summarise(mlapse = mean(dmean),slapse =mean(dsd))
+
+
+
+
