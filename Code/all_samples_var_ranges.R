9f421de917c2f2579ac7807409765ede982d2765
Abby <aby.lute@gmail.com>
2018-05-15 10:31:50 -0700

    Updated to work with any temperature type (tmax/tmin/tmean)

--- Code/all_samples_var_ranges.R
+++ Code/all_samples_var_ranges.R
@@ -1,21 +1,25 @@
-all_samples_var_ranges2 <- function(sampcol, meta., meta_sc., vars.){
+all_samples_var_ranges <- function(sampcol, meta., meta_sc., vars.){
   library(tidyverse)
   
-  samp_vars <- (matrix(nrow=1, ncol=(length(vars.)+1+5+5))) # set up an table with the ranges of each variable for each sample
-  samp_var_names <- c('num_stations',vars.,'Annual','Spring','Summer','Fall','Winter','Annual_rmse','Spring_rmse','Summer_rmse','Fall_rmse','Winter_rmse')
-  samp_vars[1] <- length(sampcol) # this is the number of stations
+  samp_vars <- (matrix(nrow=(length(unique(meta.$season))*length(vars.)), ncol=6)) # set up an table with the ranges of each variable for each sample
+  samp_var_names <- c('num_stations','season','lapse','rmse','var','var_range')
+  samp_vars[,1] <- length(sampcol) # this is the number of stations
+  samp_vars[,2] <- rep(unique(as.character(meta.$season)), each=length(vars.))
+  samp_vars[,5] <- rep(vars., length(unique(meta.$season)))
   cc <- which(names(meta.) %in% vars.)
   
   tt <- meta_sc.[which(meta_sc.$station_id %in% sampcol),]
+  var_rngs <- numeric(length(vars.))
   for (vv in 1:length(vars.)){
-    samp_vars[(vv+1)] <- diff(range(tt[,cc[vv]])) # because I'm using the range function, there's no need to use the abs value, it does this (indirectly) automatically
+    var_rngs[vv] <- diff(range(tt[,cc[vv]])) # because I'm using the range function, there's no need to use the abs value, it does this (indirectly) automatically
   }
-  df_small <- meta. %>% filter(station_id %in% sampcol) %>% group_by(season, station_id, elev) %>% summarise(mt = mean(tmax))
+  samp_vars[,6] <-rep(var_rngs, length(unique(meta.$season)))
+  
+  df_small <- meta. %>% filter(station_id %in% sampcol) %>% group_by(season, station_id, elev) %>% summarise(mt = mean(temp))
   ls <- df_small %>% group_by(season) %>% summarise(lapse = summary(lm(mt~elev))$coefficients[2]*1000,
                                                     rmse = sqrt(sum(summary(lm(mt~elev))$residuals^2)/length(sampcol)))
-  samp_vars[(length(vars)+2):(length(vars)+6)] <- ls$lapse[match(samp_var_names[(length(vars)+2):(length(vars)+6)], as.character(ls$season))]
-  
-  samp_vars[(length(vars)+7):(length(vars)+11)] <- ls$rmse[match(samp_var_names[(length(vars)+2):(length(vars)+6)], as.character(ls$season))]
+  samp_vars[,3] <- rep(ls$lapse[match(unique(as.character(meta.$season)), as.character(ls$season))], each=length(vars.))
+  samp_vars[,4] <- rep(ls$rmse[match(unique(as.character(meta.$season)), as.character(ls$season))], each=length(vars.))
   
   
   
