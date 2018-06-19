72b627b1ff7ee4718252c7ee1d54d2747b733575
Abby <aby.lute@gmail.com>
2018-06-19 09:20:15 -0700

    Added slope, aspect, cloud cover, landcover, free-air temperatures, and the waterbody index

--- Code/prep_station_data.R
+++ Code/prep_station_data.R
@@ -15,21 +15,47 @@ prep_station_data = function(data, save_dir=F){
     print('Error: dataframe called data must either contain a month column or a season column')
   }
   
-  trim_data <- data %>% dplyr::select(station_id,lat,lon,elev) %>% distinct()
+  library(raster)
   
+  
+  trim_data <- data %>% group_by(add=F) %>% dplyr::select(station_id,lat,lon,elev)  %>% distinct()
+  class(trim_data$lat) <- 'numeric'
+  class(trim_data$lon) <- 'numeric'
+  class(trim_data$elev) <- 'numeric'
+  
+  
+  # Get TPI values:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   print('Grabbing TPI values...')
   
-  # Get TPI values:
   source('Code/calculate_station_tpi.R')
   buffers <- c(200,500,1000,5000)
   tpis <- calculate_station_tpi(trim_data,buffers)
-  tpis <- cbind(trim_data$station_id,tpis); names(tpis)[1] <- 'station_id'
+  tpis <- cbind(trim_data$station_id,tpis,stringsAsFactors=F); names(tpis)[1] <- 'station_id'
   data <- left_join(data,tpis,by='station_id')
+  rm(tpis,buffers); gc()
   
+  
+  # Get Aspect and Slope::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  print('Grabbing Aspect and Slope values...')
+  
+  rast <- brick('Data/DEM/WUS_DEM_slope_aspect.tif')
+  
+  myPoints <- data.frame(trim_data$lon, trim_data$lat)
+  coordinates(myPoints) <- c('trim_data.lon', 'trim_data.lat')
+  proj4string(myPoints) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84") # proj4string(rast)
+  
+  out <- as.data.frame(raster::extract(rast, myPoints)) 
+  out <- bind_cols('station_id' = trim_data$station_id,out); names(out) <- c('station_id','slope','aspect')
+  data <- left_join(data,out,by='station_id')
+  
+  rm(rast,out); gc()
+  
+  
+  # Get seasons:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   print('Grabbing seasons...')
-  # Get seasons:
+
   if ('season' %in% names(data)){
-    
+    print('data already has seasons')
   } else {
     if (is.numeric(data$month) | nchar(data$month[1])==2){
       seasons <- data.frame('Fall'=c('09','10','11'), 'Winter'=c('12','01','02'),'Spring'=c('03','04','05'),
@@ -49,45 +75,109 @@ prep_station_data = function(data, save_dir=F){
   #tm$year[which(tm$month==12)] <- as.character(as.numeric(tm$year[which(tm$month==12)]) +1)
   #tm <- tm %>% filter(!(year==2017 & month==12)) # remove the december 2016 observations which would be grouped with winter 2017 since we don't have all the data for winter 2017
   
+  
+  # Get solar radiation::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   print('Grabbing solar radiation values...')
-  # Get solar radiation:
+
   source('Code/get_station_srad.R')
   srad <- get_station_srad(trim_data) 
-  srad <- cbind(trim_data$station_id,srad); names(srad)[1] <- 'station_id'
-  
-  # convert monthly srad to seasonal srad:
-  srad_seasonal <- srad %>% group_by(station_id) %>% 
-    summarise(Winter = mean(c(jan,feb,dec),na.rm=T), Spring = mean(c(mar,apr,may),na.rm=T),
-              Summer = mean(c(jun,jul,aug),na.rm=T), Fall = mean(c(sep,oct,nov),na.rm=T)) %>%
-    gather('season','srad',c('Winter','Spring','Summer','Fall'))
-  
+
   if ('srad' %in% names(data)){ # in case there is an existing 'srad' column in the dataframe
     names(data)[which(names(data)=='srad')] <- 'srad_orig'
   }
   
-  data <- left_join(data,srad_seasonal, by=c('station_id','season'))
+  data <- left_join(data,srad, by=c('station_id','lat','lon','season'))
+  rm(srad); gc()
   
+  
+  # Get windices::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   print('Grabbing windices...')
-  # Get windices:
+
   #source('Code/get_wind_index.R')
   winddir <- 'Data/Windex/'
   buffer <- c(500,1000,5000,10000,20000,40000)# in meters
+  tic('ready go')
   for (bb in 1:length(buffer)){
+    print(paste0('running ',buffer[bb],'m buffer'))
+    tic('start buffer')
     wind_sub <- read.table(paste0(winddir,'windex_',buffer[bb],'.txt'), stringsAsFactors=F)
+    wind_sub <- wind_sub %>% filter(station_id %in% trim_data$station_id)
     if (!bb == 1){
       winds <- left_join(winds,wind_sub, by=c('station_id','season','year'))
     } else {
       winds <- wind_sub
     }
+    toc()
   }
-  write.table(winds, 'Data/Windex/windices_TopoWx.txt')
+  toc()
   
-  #wi <- get_wind_index(data,buffers)
-  #class(wi$year) <- 'integer'
   data <- left_join(data,winds,by=c('station_id','season','year'))
+  rm(winddir,buffer,wind_sub,winds,bb); gc()
   
   
+  # Get cloud cover::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  print('Grabbing cloud cover...')
+  cdir <- 'Data/MODIS_clouds/'
   
+  seas <- unique(data$season)
+  clouds <- data.frame(matrix(nrow=nrow(trim_data), ncol=length(seas))); names(clouds) <- seas
+  for (ss in 1:length(seas)){
+    print(seas[ss])
+    rast <- raster(paste0(cdir,tolower(seas[ss]),'_cloudcover.tif'))
+    clouds[,ss] <- raster::extract(rast, myPoints)
+  }
+  clouds <- bind_cols(trim_data,clouds)
+  clouds <- clouds %>% gather('season','cloudcover',seas)
+  
+  data <- left_join(data,clouds, by=c('station_id','lat','lon','elev','season'))
+  rm(cdir,clouds,rast,ss); gc()
+  
+  
+  # Get land cover::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  print('Grabbing land cover...')
+  
+  rast <- brick('Data/EarthEnv_landcover/WUS_landcover.tif')
+  landcover <- as.data.frame(raster::extract(rast, myPoints)) 
+  names(landcover) <- paste0('landcover_',c(1:12))
+  landcover <- bind_cols(trim_data, landcover)              
+  
+  data <- left_join(data, landcover, by=c('station_id','lat','lon','elev'))
+  rm(rast,landcover); gc()
+  
+  
+  # Get waterbody distance index ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  print('Grabbing water body distance index...')
+  source('Code/prep_waterbody_layer.R')
+  
+  water <- prep_waterbody_layer(trim_data)
+  
+  data <- left_join(data,water, by=c('station_id','lat','lon','elev'))
+  rm(water); gc()
+  
+  
+  # Get free-air temperature ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  print('Grabbing free-air temperature...')
+  
+  fatemp <- get(load('Data/era-i-heights/free_air_temperature_yearly_seasonally.RData'))
+  # note the above file was created in the script 'get_free_air_adjusted_station_temp...'
+  fa_lons <- unique(fatemp$lon)
+  fa_lats <- unique(fatemp$lat)
+  
+  stations <- trim_data %>% group_by(station_id,lat,lon) %>% mutate(fa_lon = fa_lons[which.min(abs(fa_lons-lon))],
+                                                                   fa_lat = fa_lats[which.min(abs(fa_lats-lat))])
+  stations <- stations %>% group_by(add=F) %>% select(station_id,fa_lon,fa_lat)
+  fa_master <- left_join(stations,fatemp,by=c("fa_lat"="lat", "fa_lon"="lon")) %>% select(-fa_lon,-fa_lat)
+  class(fa_master$station_id) <- 'character'
+  class(fa_master$year) <- 'integer'
+  class(fa_master$season) <- 'character'
+  
+  data <- left_join(data,fa_master, by=c('station_id','year','season'))
+  data$fa_adj_temp <- data$temp-data$fatemp
+  rm(fatemp,fa_lons,fa_lats,stations,fa_master); gc()
+    
+    
+    
+  #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   
   if (!save_dir == F){
     write.table(data, save_dir)
